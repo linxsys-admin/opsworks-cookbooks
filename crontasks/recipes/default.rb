fde8c6854a5570baba18c6534af2f1495058f331
tom <tom@skibinski.it>
2015-04-16 16:11:40 +0200

    Removing s3_sync cron task

--- crontasks/recipes/default.rb
+++ crontasks/recipes/default.rb
@@ -3,23 +3,6 @@
 # recipe:: default
 #
 
-template "/root/s3_sync.sh" do
-  source "s3_sync.sh.erb"
-  mode "0755"
-  owner "root"
-  group "root"
-  variables(
-    :bucket_name => node[:crontasks][:bucket_name]
-  )
-  action :create
-end
-
-cron "s3_sync" do
- minute "*/5"
- user "root"
- command "/root/s3_sync.sh"
-end
-
 cron "wp_cron" do
   minute "*/15"
   user "root"
