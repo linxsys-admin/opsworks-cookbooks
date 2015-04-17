0c881e41b0980a3a67156fb9c96bb256a785512b
tom <tom@skibinski.it>
2015-04-17 14:59:20 +0200

    Update for configuration template

--- postfix-ses/recipes/default.rb
+++ postfix-ses/recipes/default.rb
@@ -7,41 +7,41 @@ package "postfix" do
   action :install
 end
 
-service 'postfix' do
-  service_name 'postfix'
-  action :enable
+
+template "/etc/postfix/main.cf" do
+  source "main.cf.erb"
+  mode 0644
+  owner "root"
+  group "root"
+  variables(
+    :relayhost => node[:postfix_ses][:relayhost],
+    :hostname => node[:opsworks][:instance][:hostname]
+  )
+  action :create
 end
 
-script "update_main.cf_file" do
+template "/etc/postfix/sasl_passwd" do
+  source "sasl_passwd.erb"
+  mode 0644
+  owner "root"
+  group "root"
+  variables(
+    :relayhost => node[:postfix_ses][:relayhost],
+    :username => node[:postfix_ses][:username],
+    :password => node[:postfix_ses][:password]
+  )
+  action :create
+end
+
+script "create_a_hashmap_database" do
   interpreter "bash"
   user "root"
   code <<-EOH
-    sed -i "s/^default_transport.*/default_transport = smtp/" /etc/postfix/main.cf
-    sed -i "s/^relay_transport.*/relay_transport = relay/" /etc/postfix/main.cf
-    sed -i "s/^relayhost.*/#relayhost = /" /etc/postfix/main.cf
-    cat<<_EOF>>/etc/postfix/main.cf
-    relayhost = #{node[:postfix_ses][:relayhost]}:25
-    smtp_sasl_auth_enable = yes
-    smtp_sasl_security_options = noanonymous
-    smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd
-    smtp_use_tls = yes
-    smtp_tls_security_level = encrypt
-    smtp_tls_note_starttls_offer = yes
-    smtp_tls_CAfile = /etc/ssl/certs/ca-certificates.crt
-    _EOF_
-  EOH
-end
-
-script "sasl_passwd_smtp_credentials" do
-  interpreter "bash"
-  user "root"
-  code <<-EOH
-    echo "#{node[:postfix_ses][:relayhost]}:25 #{node[:postfix_ses][:username]}:#{node[:postfix_ses][:password]}" > /etc/postfix/sasl_passwd
-    postmap /etc/postfix/sasl_passwd
+    postmap hash:/etc/postfix/sasl_passwd
     rm -f /etc/postfix/sasl_passwd
   EOH
 end
 
 service "postfix" do
-  action :restart
+  action [ :enable, :restart ]
 end
