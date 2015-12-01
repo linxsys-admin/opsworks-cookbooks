39d6c40ea98759b54b84e6d888d99d9cd5b685bb
Oleg Dubyk <oleg.dubyk@linxsys.pl>
2015-12-01 17:39:57 +0200

    add recipe for cleaning previously uploaded files

---
+++ crontasks/recipes/clean_uploaded_files.rb
@@ -0,0 +1,13 @@
+#
+# cookbook:: crontasks
+# recipe:: clean_uploaded_files
+#
+
+cron "wp_cron_clean" do
+  minute "0"
+  hour "0"
+  user "root"
+  node[:deploy].each do |app, deploy|
+    command "find #{node[:deploy][app][:deploy_to]}/shared/uploads/{2010..2016}/{1..12}/ -type f -mtime +1 -exec rm -f {} \;"
+  end
+end
