2b04a2323730ed3d8a99c032193ecdfbbc0089bb
Oleg Dubyk <oleg.dubyk@linxsys.pl>
2015-12-10 15:14:03 +0200

    modify clean_uploaded_files, create separate script

--- crontasks/recipes/clean_uploaded_files.rb
+++ crontasks/recipes/clean_uploaded_files.rb
@@ -3,11 +3,21 @@
 # recipe:: clean_uploaded_files
 #
 
-cron "wp_cron_clean" do
-  minute "0"
-  hour "0"
-  user "root"
-  node[:deploy].each do |app, deploy|
-    command "find #{node[:deploy][app][:deploy_to]}/shared/uploads/20??/{01..12}/ -type f -mtime +1 -exec rm -f {} \\;"
+node[:deploy].each do |app, deploy|
+
+  file "#{deploy[:deploy_to]}/shared/clean.sh" do
+    content %Q|#!/bin/bash
+find #{deploy[:deploy_to]}/shared/uploads/20??/{01..12}/ -type f -mtime +1 -exec rm -f {} \\;
+|
+    mode "0755"
+    owner "root"
+  end
+
+  cron "wp_cron_clean" do
+    minute "0"
+    hour "0"
+    user "root"
+    command "#{deploy[:deploy_to]}/shared/clean.sh"
   end
 end
+
